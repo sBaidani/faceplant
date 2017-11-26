9b12aa202ac3e0b7e167009180769fa7761c8f46
Sadiq Baidani <sadiq786baidani@gmail.com>
2017-11-26 04:32:45 +0000

    Holy shit we're geniuses!

--- emotion.js
+++ emotion.js
@@ -1,19 +1,16 @@
 function compareImages(img1, img2)
 {
     var results = new Array(8);
-    var substr = new Array(2);
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < img1[i].length; j++)
-        {
-            substr.append(img1[i].substring(":", img1[i].length));
-        }
-    }
-    console.log(substr);
-    for (i = 0; i < 8; i++)
-    {
-        results[i] = img1[i] - img2[i];
-    }
+    
+    results[0] = img1.anger - img2.anger; 
+    results[1] = img1.contempt - img2.contempt; 
+    results[2] = img1.disgust - img2.disgust; 
+    results[3] = img1.fear - img2.fear; 
+    results[4] = img1.happiness - img2.happiness; 
+    results[5] = img1.neutral - img2.neutral; 
+    results[6] = img1.sadness - img2.sadness; 
+    results[7] = img1.surpise - img2.surpise; 
+
     var max = 0;
     var index = -1;
     for (i = 0; i < 8; i++)
@@ -24,6 +21,5 @@ function compareImages(img1, img2)
             index = i;
         }
     }
-    var rtn = "" + index.toString() + ": " + max.toString();
-    return 
+    return index.toString();
 }
