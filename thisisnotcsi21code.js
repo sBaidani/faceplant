b61a90b21ce7f1bb1f94ee18b80b87d1538c083d
Simona Ifeji <csi13@imperial.ac.uk>
2017-11-26 12:09:39 +0000

    final push

---
+++ thisisnotcsi21code.js
@@ -0,0 +1,39 @@
+/*<!--
+AUTHOR: @paulrouget <paul@mozilla.com>
+LICENSE:
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+0. You just DO WHAT THE FUCK YOU WANT TO. -->
+
+
+<!-- One single file. This one. -->
+this code is by PaulRouget (Mozilla)*/
+/* Drag'n drop stuff */
+    window.ondragover = function(e) {e.preventDefault()}
+    window.ondrop = function(e) {e.preventDefault(); upload(e.dataTransfer.files[0]); }
+    function upload(file) {
+        /* Is the file an image? */
+        if (!file || !file.type.match(/image.*/)) return;
+        /* It is! */
+        document.body.className = "uploading";
+        /* Lets build a FormData object*/
+        var fd = new FormData(); // I wrote about it: https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
+        fd.append("image", file); // Append the file
+        fd.append("key", "6528448c258cff474ca9701c5bab6927"); // Get your own key http://api.imgur.com/
+        var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+        xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
+        xhr.onload = function() {
+            // Big win!
+            document.querySelector("#link").href = JSON.parse(xhr.responseText).upload.links.imgur_page;
+            document.body.className = "uploaded";
+        }
+        // Ok, I don't handle the errors. An exercice for the reader.
+        /* And now, we send the formdata */
+        xhr.send(fd);
+    }
+
+    /*Look at the source code for more information. By <a href="http://twitter.com/paulrouget">@paulrouget*/
