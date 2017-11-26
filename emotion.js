c08162e533ee6898e7e6d323fdb7490341812f17
Sadiq Baidani <sadiq786baidani@gmail.com>
2017-11-26 03:12:30 +0000

    Fixed a shit tonne of stuff

--- emotion.js
+++ emotion.js
@@ -1,13 +1,22 @@
 function compareImages(img1, img2)
 {
-    var results = [8];
-    for (i = 0; i <= 8; i++)
+    var results = new Array(8);
+    var substr = new Array(2);
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < img1[i].length; j++)
+        {
+            substr.append(img1[i].substring(":", img1[i].length));
+        }
+    }
+    console.log(substr);
+    for (i = 0; i < 8; i++)
     {
         results[i] = img1[i] - img2[i];
     }
     var max = 0;
     var index = -1;
-    for (i = 0; i <= 8; i++)
+    for (i = 0; i < 8; i++)
     {
         if (Math.abs(results[i]) > max)
         {
@@ -15,4 +24,6 @@ function compareImages(img1, img2)
             index = i;
         }
     }
+    var rtn = "" + index.toString() + ": " + max.toString();
+    return 
 }
