56115247eb0052fb19184a6ad3e54b0cc3d7c3aa
Simona I <csi13@imperial.ac.uk>
2017-11-26 01:31:02 +0000

    Update Faceplant.js

--- Faceplant.js
+++ Faceplant.js
@@ -1,2 +1,12 @@
 
+/*this is the JS file*/
+var header = document.querySelector('.header');
+var origOffsetY = header.offsetTop;
+
+function onScroll(e) {
+  window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+                                  header.classList.remove('sticky');
+}
+
+document.addEventListener('scroll', onScroll);
     
